6da85236fac06821dad23a1e9b9a7d94a946e7ce
sohel <shksohel03@gmail.com>
2023-07-19 00:58:35 +0530

    testing

--- test/test.js
+++ test/test.js
@@ -2,9 +2,9 @@ var request = require('supertest');
 
 var app = require('../index.js');describe('GET /', function() {
 
- it('respond with hello sabko', function(done) { //navigate to root and check the the response is "hello world"
+ it('respond with hello sabko', function(done) { //navigate to root and check the the response is "hello sabko"
 
- request(app).get('/').expect('hello all of you', done);
+ request(app).get('/').expect('hello sabko', done);
 
  });
 
